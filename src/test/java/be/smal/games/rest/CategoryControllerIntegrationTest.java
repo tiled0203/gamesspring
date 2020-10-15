c2ca5f9709d660aac3c624a4e54576d63a36b564
Jorne <jorne.devos@gmail.com>
2020-10-15 11:11:30 +0200

    can't figure out the reset on auto increment in the categoryControllerIntegrationTest ckass

--- src/test/java/be/smal/games/rest/CategoryControllerIntegrationTest.java
+++ src/test/java/be/smal/games/rest/CategoryControllerIntegrationTest.java
@@ -4,6 +4,7 @@ package be.smal.games.rest;
 import be.smal.games.services.CategoryService;
 import be.smal.games.domain.Category;
 import be.smal.games.domaindto.CategoryDTO;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,9 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.springframework.transaction.annotation.Transactional;
+
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
-public class CategoryControllerTest {
+public class CategoryControllerIntegrationTest {
 
     @Autowired
     CategoryController categoryController;
@@ -21,8 +26,21 @@ public class CategoryControllerTest {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    EntityManager entityManager;
+
+    //todo   trying to reset the auto increment jpa is applying as for my db not to have a huge gap in id numbers all of a sudden
+/*
+    @After
+    public  void resetAutoIncrement() {
+        int size = categoryService.findAll().size() + 1;
+        Query query = entityManager.createNativeQuery("ALTER TABLE student AUTO_INCREMENT = " + size);
+        query.executeUpdate();
+    }
+*/
+
     @Test
-   public void addCategoryTest(){
+   public void addCategoryThenRemoveTest(){
         CategoryDTO categoryDTO = new CategoryDTO("testName");
         Category category = categoryController.saveCategory(categoryDTO);
         Assert.assertNotNull(category);
