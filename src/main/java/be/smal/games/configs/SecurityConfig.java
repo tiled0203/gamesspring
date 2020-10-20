ca1db359e93931e2187b029b8a2e57951f21087c
TLUBB59 <tiled0203@gmail.com>
2020-10-20 15:34:47 +0200

    rolled back SecurityConfig

--- src/main/java/be/smal/games/configs/SecurityConfig.java
+++ src/main/java/be/smal/games/configs/SecurityConfig.java
@@ -1,45 +1,42 @@
 package be.smal.games.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth
                 .inMemoryAuthentication()
                 .withUser("user")
-                .password(encoder().encode("user"))
+                .password(encoder.encode("user"))
                 .roles("USER")
                 .and()
                 .withUser("root")
-                .password(encoder().encode("root"))
+                .password(encoder.encode("root"))
                 .roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and()
+        http
                 .authorizeRequests()
                 //playing with options
-                .antMatchers("/game/*", "/category").hasRole("ADMIN")
-                .antMatchers("/login", "/*").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().and().csrf().disable();
+                .antMatchers("/game*").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
 
